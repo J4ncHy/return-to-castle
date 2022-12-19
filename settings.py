e33803b25dbe80e4b8a99ae20c5f43df5daa283c
J4ncHy <janmrak1@gmail.com>
2022-12-19 11:21:56 +0100

    Added bullet class with animation - not yet tested

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ level_map = [
     '                                                                                                        ',
     '                                                                                                   S    ',
     '                                               X                                            XXXXXXXX    ',
-    '   P      E   X        S                       X                              XXX   XXXX                ',
+    '   P    X E X X        S                       X                              XXX   XXXX                ',
     '  XXXXX  XXX  XXX    XXX    XX   XXX          XX     XXXXX       X      X                               ',
     '                                        XXX    X               XXXXXXXXXX                               ',
     '                                                                                                        ',
