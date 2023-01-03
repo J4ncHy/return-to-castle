6aeecdb2a6ec54561ecd5b492cda620228a1972b
Jan Mrak <janmrak1@gmail.com>
2023-01-03 20:44:06 +0100

    refactor: made powerups code nicer

--- PowerUps.py
+++ PowerUps.py
@@ -1,12 +1,18 @@
 import pygame
 
+
 class Powerups(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        #powerups = import_folder("media/powerups/")
-        self.image = pygame.image.load("media/powerups/speedup.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (64, 64))
+        self.image = None
+        self.import_sprite()
+
         self.rect = self.image.get_rect(topleft=pos)
+
+    def import_sprite(self):
+        path = "media/powerups/speedup.png"
+        self.image = pygame.image.load(path).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (64, 64))
 
     def update(self, x_shift):
         self.rect.x += x_shift
